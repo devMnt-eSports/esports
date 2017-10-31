aa3e362b2e80f7fe49b7d1690cb0b619dadedfa3
Stewart Atwood <stewartatwood@Stewarts-MacBook-Pro.local>
2017-10-31 12:24:19 -0500

    hopefully it will have been worth the wait

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        /* Navbar will be on every page, so it stays in the main App.js, no need
+        to make a separate component */
         <nav>
           <ul id="nav-bar">
             <li>Home</li>
@@ -13,6 +15,7 @@ class App extends Component {
             <li>Login</li>
           </ul>
         </nav>
+        /* Below copied to file components/home/leftnav */
         <div className="content-distribution">
           <div className="wrapper">
             <input type="checkbox" id="testToggler" className="input-toggler" />
@@ -34,9 +37,11 @@ class App extends Component {
             </div>
           </div>
           <div className="middle-feed">
+            /* Below copied to components/home/homesplash */
             <div>
               <h1 id="main-feed-head">Hottest News!</h1>
             </div>
+            /* Below copied to components/home/homefeed */
             <div className="example-article">
               <div id="article-head">
                 <h1>Behold the Splash Article!</h1>
@@ -60,6 +65,7 @@ class App extends Component {
             </div>
           </div>
           <div className="social-media">
+            /* Below copied to components/home/twittefeed */
             <div>
               <h3>Twitter Feed:</h3>
               <p>
